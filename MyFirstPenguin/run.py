c3fea4f4e41158b3c5c758ff5ede80fd4e858c4e
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2018-11-07 18:09:38 +0100

    Fixed error 500! I think

--- MyFirstPenguin/run.py
+++ MyFirstPenguin/run.py
@@ -98,10 +98,8 @@ def findNearestCorner(body):
     top_right_distance = math.sqrt((body["mapWidth"] - xValuePlayer) ** 2 + yValuePlayer ** 2)
     bottom_left_distance = math.sqrt(xValuePlayer ** 2 + (body["mapHeight"] - yValuePlayer) ** 2)
     bottom_right_distance = math.sqrt((body["mapWidth"] - xValuePlayer) ** 2 + (body["mapHeight"] - yValuePlayer) ** 2)
-    print(top_left_distance, top_right_distance, bottom_left_distance, bottom_right_distance)
     choices = [(0, 0, top_left_distance), (body["mapWidth"], 0, top_right_distance), (0, body["mapHeight"], bottom_left_distance), (body["mapWidth"], body["mapHeight"], bottom_right_distance)]
-    print(choices)
-    return choices.sort(key=lambda tup: tup[2])[0]
+    return sorted(choices, key=lambda tup: tup[2])[0]
 
 # --------------- Move-methods ---------------
 def moveTowardsPoint(body, pointX, pointY):
@@ -225,7 +223,7 @@ response = {}
 returnObject = {}
 if req_params_query == "info":
     returnObject["name"] = "Pingu"
-    returnObject["team"] = "Team Python"
+    returnObject["team"] = "Error 500"
 elif req_params_query == "command":    
     body = json.loads(open(env["req"], "r").read())
     returnObject["command"] = chooseAction(body)
