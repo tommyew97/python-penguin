42962a734859eec1ed27f5b5e1454818abb5d4ac
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2018-11-07 18:13:14 +0100

    Fixed error by one in findNearestCorner

--- MyFirstPenguin/run.py
+++ MyFirstPenguin/run.py
@@ -95,10 +95,10 @@ def findNearestCorner(body):
     xValuePlayer = body["you"]["x"]
     yValuePlayer = body["you"]["y"]
     top_left_distance = math.sqrt(xValuePlayer ** 2 + yValuePlayer ** 2)
-    top_right_distance = math.sqrt((body["mapWidth"] - xValuePlayer) ** 2 + yValuePlayer ** 2)
-    bottom_left_distance = math.sqrt(xValuePlayer ** 2 + (body["mapHeight"] - yValuePlayer) ** 2)
-    bottom_right_distance = math.sqrt((body["mapWidth"] - xValuePlayer) ** 2 + (body["mapHeight"] - yValuePlayer) ** 2)
-    choices = [(0, 0, top_left_distance), (body["mapWidth"], 0, top_right_distance), (0, body["mapHeight"], bottom_left_distance), (body["mapWidth"], body["mapHeight"], bottom_right_distance)]
+    top_right_distance = math.sqrt((body["mapWidth"] - 1 - xValuePlayer) ** 2 + yValuePlayer ** 2)
+    bottom_left_distance = math.sqrt(xValuePlayer ** 2 + (body["mapHeight"] - 1 - yValuePlayer) ** 2)
+    bottom_right_distance = math.sqrt((body["mapWidth"] - 1 - xValuePlayer) ** 2 + (body["mapHeight"] - 1 - yValuePlayer) ** 2)
+    choices = [(0, 0, top_left_distance), (body["mapWidth"] - 1, 0, top_right_distance), (0, body["mapHeight"] - 1, bottom_left_distance), (body["mapWidth"] - 1, body["mapHeight"] - 1, bottom_right_distance)]
     return sorted(choices, key=lambda tup: tup[2])[0]
 
 # --------------- Move-methods ---------------
